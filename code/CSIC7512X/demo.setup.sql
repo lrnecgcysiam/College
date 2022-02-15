737d3a97e0a4377930bd557f4881414b9d635122
Alayode Auroraixion <chris@kayode.me>
2022-02-15 15:00:22 -0500

    created script

--- code/CSIC7512X/demo.setup.sql
+++ code/CSIC7512X/demo.setup.sql
@@ -1,55 +1,48 @@
-DROP table patron;
-DROP table beer;
+-- Drop tables if they exist with same name
+DROP table account;
+DROP table customer;
 DROP table transaction;
-DROP table transactionitem;
 
---- Enter the Patron Table
-
-create table patron(patronid int,
-    nickname varchar(50),
+-- Create a tables
+create table customer(customerid int,
+    username varchar(50),
     fname varchar(50),
-    lname varchar(65)
-);
-
---- Enter the Transaction Table
-
-create table transaction(txid int,
-    patronid int,
-    event_tim varchar(12)
-);
-
---Enter The Transactionitem table
-
-create table transactionitem(txid int,
-    beerid int,
-    qty int,
-    price int
-);
-
---- beer 
-create table beer(beerid int,
-    description varchar(255),
-    listedprice int
+    lname varchar(65),
+    street varchar(65),
+    street2 varchar(65),
+    city varchar(65),
+    state varchar(50),
+    zip int
 );
 
 
-COPY patron(patronid, nickname, fname, lname)
-From '/home/pi/dbsys/finaldb/patron.MOCK_DATA.csv'
+create table account(
+    accountid varchar(10),
+    customerid varchar(10),
+    description varchar(50)
+);
+
+
+create table transaction(
+    transactionid varchar(10),
+    trantimestamp varchar(50),
+    accountid varchar(10),
+     amount(int)
+);
+
+
+-- Populate the tables with csv files
+COPY customer(customerid, username, fname, lname, street, street2, city, state,)
+From '/home/pi/College/code/CSIC7512X/CUSTOMERS.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY beer(beerid, description, listedprice)
-From '/home/pi/dbsys/finaldb/beer.MOCK_DATA.csv'
+COPY account(accountid, customerid, description)
+From '/home/pi/College/code/CSIC7512X/ACCOUNTS.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY transaction(txid, patronid, event_tim)
-From '/home/pi/dbsys/finaldb/transaction.MOCK_DATA.csv'
+COPY transaction(transactionid, trantimestamp, accountid, amount)
+From '/home/pi/College/code/CSIC7512X/TRANSACTIONS.csv'
 DELIMITER ','
 CSV HEADER;
-
-COPY transactionitem(txid, beerid, qty, price)
-From '/home/pi/dbsys/finaldb/transactionitem.MOCK_DATA.csv'
-DELIMITER ','
-CSV HEADER;
-
