0d1293ab5020d630e9e3536796e2cb82074c4e66
Alayode Auroraixion <chris@kayode.me>
2022-03-31 02:56:08 -0400

    update

--- code/CISC7512X/coursework/midtermPrep/midterm20191030.sql
+++ code/CISC7512X/coursework/midtermPrep/midterm20191030.sql
@@ -7,8 +7,8 @@ DROP table journal;
 
 create table customer(cid int,
     fname varchar(50),
-    lname varchar(65)
-    ssn varchar(50),
+    lname varchar(65),
+    ssn varchar(9),
 );
 
 --- Enter the Account Table
@@ -21,7 +21,7 @@ create table account(aid int,
 -- timestamp 19700131 - 20220401
 create table journal(jid int,
     tid int,
-    tim varchar(),
+    tim varchar(8),
     aid int,
     pid int,
     qty int
